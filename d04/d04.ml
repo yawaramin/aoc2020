82b20b14fb4b3c5a255b67d20e945d6e2a8e4252
Yawar Amin <yawar.amin@gmail.com>
2020-12-05 01:00:49 -0500

    Day 4 part 1

---
+++ d04/d04.ml
@@ -0,0 +1,55 @@
+type partial_passport = {
+  byr : string;
+  iyr : string;
+  eyr : string;
+  hgt : string;
+  hcl : string;
+  ecl : string;
+  pid : string;
+}
+
+let empty_passport = {
+  byr = "";
+  iyr = "";
+  eyr = "";
+  hgt = "";
+  hcl = "";
+  ecl = "";
+  pid = "";
+}
+
+let valid_passport { byr; iyr; eyr; hgt; hcl; ecl; pid } =
+  byr <> "" &&
+  iyr <> "" &&
+  eyr <> "" &&
+  hgt <> "" &&
+  hcl <> "" &&
+  ecl <> "" &&
+  pid <> ""
+
+let fill_partial partial field = match String.split_on_char ':' field with
+  | [_; ""] -> partial
+  | ["byr"; byr] -> { partial with byr }
+  | ["iyr"; iyr] -> { partial with iyr }
+  | ["eyr"; eyr] -> { partial with eyr }
+  | ["hgt"; hgt] -> { partial with hgt }
+  | ["hcl"; hcl] -> { partial with hcl }
+  | ["ecl"; ecl] -> { partial with ecl }
+  | ["pid"; pid] -> { partial with pid }
+  | _ -> partial
+
+let count_valid (count, partial) = function
+  | "" ->
+    if valid_passport partial then succ count, empty_passport
+    else count, empty_passport
+  | line ->
+    let fields = String.split_on_char ' ' line in
+    count, List.fold_left fill_partial partial fields
+
+let () =
+  let num_valid, _ =
+    Lib.fold_file_lines "input" count_valid (0, empty_passport)
+  in
+  Printf.printf "Part 1: %d\n" num_valid
+
+(* Part 1: 222 *)
