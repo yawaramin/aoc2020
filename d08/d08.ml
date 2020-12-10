e1056693763c3027b4840ae865d4b3239031a357
Yawar Amin <yawar.amin@gmail.com>
2020-12-09 22:56:57 -0500

    Day 8 part 2

--- d08/d08.ml
+++ d08/d08.ml
@@ -3,7 +3,7 @@ type instruction = op * int
 
 let size = 628
 let program = Array.make size (Nop, 0)
-let history = Array.make size false
+let history () = Array.make size false
 
 let instruction_of_string line = match String.split_on_char ' ' line with
   | [op; num] ->
@@ -34,27 +34,38 @@ let eval ~ptr ~acc = function
   | Acc, amt -> succ ptr, acc + amt
   | Jmp, amt -> ptr + amt, acc
 
-let rec run ?flip (ptr, acc) =
-  if not (history.(ptr)) && ptr < size then begin
+let rec run ?flip (ptr, acc) history =
+  if ptr < size && not (history.(ptr)) then begin
     history.(ptr) <- true;
     match program.(ptr), flip with
     | (Nop, amt), Some fliptr when ptr = fliptr ->
-      run (eval ~ptr ~acc (Jmp, amt))
+      run ?flip (eval ~ptr ~acc (Jmp, amt)) history
     | (Jmp, amt), Some fliptr when ptr = fliptr ->
-      run (eval ~ptr ~acc (Nop, amt))
+      run ?flip (eval ~ptr ~acc (Nop, amt)) history
     | instruction, _ ->
-      run (eval ~ptr ~acc instruction)
+      run ?flip (eval ~ptr ~acc instruction) history
   end else
     (* Tuple of: (accumulator, did the program terminate?) *)
     acc, ptr = size
 
+let rec find_terminator ptr =
+  if ptr = size then begin
+    failwith "find_terminator: could not find instruction to flip"
+  end;
+  let history = history () in
+  match run ~flip:ptr (0, 0) history with
+  | acc, true -> acc
+  | _, false -> find_terminator (succ ptr)
+
 let () =
   ignore (Lib.fold_file_lines "input" load_instruction 0);
-  let acc_p1, _ = run (0, 0) in
+  let acc_p1, _ = run (0, 0) (history ()) in
+  let acc_p2 = find_terminator 0 in
   Printf.printf
     "Part 1: %d
-Part 2: TODO\n"
+Part 2: %d\n"
     acc_p1
+    acc_p2
 
 (* Part 1: 1766
-   Part 2: *)
+   Part 2: 1639 *)
