0805e4329d81fcecc7cdb43b2ac3b3c5e7f046aa
Yawar Amin <yawar.amin@gmail.com>
2020-12-11 21:52:21 -0500

    Day 9 part 2

--- d09/d09.ml
+++ d09/d09.ml
@@ -1,20 +1,18 @@
-let preamble_len = 25
-
 module Window : sig
   type t
 
-  val make : int array -> t
+  val make : int -> int array -> t
   val push : t -> int -> unit
   val is_sum2 : int -> t -> bool
 end = struct
-  type t = { arr : int array; mutable idx : int }
+  type t = { arr : int array; len : int; mutable idx : int }
 
-  let make arr =
-    assert (Array.length arr = preamble_len);
-    { arr; idx = 0 }
+  let make len arr =
+    assert (Array.length arr = len);
+    { arr; len; idx = 0 }
 
   let push t int =
-    let idx = succ t.idx mod preamble_len in
+    let idx = succ t.idx mod t.len in
     t.arr.(idx) <- int;
     t.idx <- idx
 
@@ -22,7 +20,9 @@ end = struct
   let is_sum2 num t = Array.exists (is_sum t.arr num) t.arr
 end
 
-let numbers = Array.make 1000 0
+let input_len = 1_000
+let preamble_len = 25
+let numbers = Array.make input_len 0
 
 let fill_numbers idx = function
   | "" ->
@@ -42,14 +42,50 @@ let rec get_first_nonsum ?(idx=preamble_len) window =
   end else
     num
 
+let sum_array = Array.fold_left ( + ) 0
+
+let rec fold_array ~result ~start ~stop f arr =
+  if start = stop then
+    result
+  else
+    fold_array ~result:(f result arr.(start)) ~start:(succ start) ~stop f arr
+
+let fold_array f arr = match Array.length arr with
+  | 0 -> failwith "fold_array: empty array"
+  | 1 -> arr.(0)
+  | len -> fold_array ~result:arr.(0) ~start:1 ~stop:len f arr
+
+let min_array = fold_array min
+let max_array = fold_array max
+
+let rec get_weakness ?(first=0) ?(last=0) num =
+  let len = last - first + 1 in
+  if last = input_len then
+    failwith "get_weakness: unable to find weakness"
+  else if len <= 0 then
+    let msg =
+      Printf.sprintf "get_weakness: invalid array slice: %d to %d" first last
+    in
+    failwith msg
+  else if len = 1 then
+    get_weakness ~first ~last:(succ last) num
+  else
+    let slice = Array.sub numbers first len in
+    let sum = sum_array slice in
+    if sum > num then get_weakness ~first:(succ first) ~last num
+    else if sum < num then get_weakness ~first ~last:(succ last) num
+    else min_array slice + max_array slice
+
 let () =
   ignore (Lib.fold_file_lines "input" fill_numbers 0);
-  let window = Window.make (Array.sub numbers 0 preamble_len) in
+  let window = Window.make preamble_len (Array.sub numbers 0 preamble_len) in
   let first_nonsum = get_first_nonsum window in
+  let weakness = get_weakness first_nonsum in
   Printf.printf
     "Part 1: %d
-Part 2: TODO\n"
+Part 2: %d\n"
     first_nonsum
+    weakness
 
 (* Part 1: 217430975
-   Part 2: TODO *)
+   Part 2: 28509180 *)
